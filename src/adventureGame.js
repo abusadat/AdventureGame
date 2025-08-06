521790bd050bfd9e39b4abbc6ff0665d91d546ac
abusadat <sadat2003217@gmail.com>
2025-08-06 10:31:31 +0600

    Initial commit - character stats setup

---
+++ src/adventureGame.js
@@ -0,0 +1,27 @@
+/*
+Adventure Game v 0.1
+This is a text based game where people can choose their option based on which the outcomes will be selected
+*/
+
+const readline = require("readline-sync");
+
+// Display the game title
+console.log("Welcome to the Adventure Game!");
+
+// Add a welcome message
+console.log("Prepare yourself for an epic journey!");
+
+let playerName = "";
+playerName = readline.question("Enter your name: ");
+
+let playerHealth = 100;
+let playerGold = 20;
+let currentLocation = "village"
+let gameRunning = true;
+let inventory = [];
+
+console.log("Hello " + playerName + ", ready for an adventure?");
+console.log("Your current gold is " + playerGold + ".");
+
+
+
