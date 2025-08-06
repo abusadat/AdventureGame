e49b50b5fe6d4b3a42539516d107247f1c999b7f
abusadat <sadat2003217@gmail.com>
2025-08-06 11:09:34 +0600

    added the variables

--- src/adventureGame.js
+++ src/adventureGame.js
@@ -1,27 +1,33 @@
-/*
-Adventure Game v 0.1
-This is a text based game where people can choose their option based on which the outcomes will be selected
-*/
+// /*
+// Adventure Game v 0.1
+// This is a text based game where people can choose their option based on which the outcomes will be selected
+// */
 
-const readline = require("readline-sync");
+// const readline = require("readline-sync");
 
-// Display the game title
-console.log("Welcome to the Adventure Game!");
+// // Display the game title
+// console.log("Welcome to the Adventure Game!");
 
-// Add a welcome message
-console.log("Prepare yourself for an epic journey!");
+// // Add a welcome message
+// console.log("Prepare yourself for an epic journey!");
 
-let playerName = "";
-playerName = readline.question("Enter your name: ");
+// console.log("=================================");
+// console.log("       The Dragon's Quest        ");
+// console.log("=================================");
+// console.log("\nYour quest: Defeat the dragon in the mountains!");
 
-let playerHealth = 100;
-let playerGold = 20;
-let currentLocation = "village"
-let gameRunning = true;
-let inventory = [];
+// let playerName = "";
+// playerName = readline.question("Enter your name: ");
 
-console.log("Hello " + playerName + ", ready for an adventure?");
-console.log("Your current gold is " + playerGold + ".");
+// let playerHealth = 100;
+// let playerGold = 20;
+// let currentLocation = "village"
+// let gameRunning = true;
+// let inventory = [];
+
+// console.log("Hello " + playerName + ", ready for an adventure?");
+// console.log("Your current gold is " + playerGold + ".");
 
 
 
+console.log('10' * 20)
