ce764036ecb068b4c43939656f0ebef34a934d58
abusadat <sadat2003217@gmail.com>
2025-08-10 21:20:07 +0600

    final

---
+++ src/test.js
@@ -0,0 +1,13 @@
+let shirtColor = "blue";
+let shirtPrice = 110;
+
+if (shirtColor == "pink") {
+    if (shirtPrice <= 100) {
+        console.log("I will buy")
+    }
+     else {
+    console.log("Color is pink, but above $100. Can't buy")
+}
+} else {
+    console.log("Color is not pink")
+}
